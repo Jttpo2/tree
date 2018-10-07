a848c0258a9f4acbcef92b0474cb0f5e4acd83ee
John Petersson <johnpetersson@gmail.com>
2018-10-07 21:40:30 +0200

    Basic recursive tree

--- sketch/tree.js
+++ sketch/tree.js
@@ -1,9 +1,8 @@
 new p5();
 
 let backgroundColor;
-let lines = [];
-
-let snowflake = null;
+let mouseMovedFromInitPosition = false;
+let tree;
 
 function setup() {
 	let canvas = createCanvas(
@@ -13,13 +12,60 @@ function setup() {
 
 	backgroundColor = color(200);
 
-	snowflake = new Snowflake(5);
+	tree = new Tree();
 }
 
 function draw() {
 	background(backgroundColor);
+	if(!mouseMovedFromInitPosition) {
+		mouseMovedFromInitPosition = mouseX != 0;
+	}
+	tree.display();
 
-	snowflake.display();
+
+}
+
+class Tree {
+	constructor() {
+		this.lines = [];
+
+		this.theta = PI;
+		this.branchLength = height/3;
+		this.aspectRatio = 1/10;
+		this.minLength = 2;
+	}
+
+	branch(length) {
+		strokeWeight(length * this.aspectRatio);
+		line(0, 0, 0, -length);
+		translate(0, -length);
+		length *= 2/3;
+		if (length < this.minLength) return;
+		push();
+		rotate(this.theta);
+		this.branch(length);
+		pop();
+		push();
+		rotate(-this.theta);
+		this.branch(length);
+		pop();
+	}
+
+	display() {
+		if (mouseMovedFromInitPosition) {
+			this.theta = map(mouseX, 0, width, 0, PI);
+		} else {
+			this.theta = PI * 1/7;
+		}
+
+		stroke(50);
+		push();
+		translate(width/2, height);
+		this.branch(this.branchLength, this.branchWidth);
+		pop();
+		this.lines.forEach(line => line.display());
+	}
+
 }
 
 function windowResized() {
