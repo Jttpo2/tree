125fe4f7c06aa5de0dbd4642bf2bb26dc38b1522
John Petersson <johnpetersson@gmail.com>
2018-10-07 18:48:23 +0200

    Cantor set

--- sketch/tree.js
+++ sketch/tree.js
@@ -6,7 +6,7 @@ function setup() {
 	let canvas = createCanvas(
 		window.innerWidth,
 		window.innerHeight
-		);
+	);
 
 	backgroundColor = color(200);
 }
@@ -14,23 +14,39 @@ function setup() {
 function draw() {
 	background(backgroundColor);
 
-	drawCircle(width / 2, height / 2, 400);
+	// drawCircle(width / 2, height / 2, 400);
+	cantor(10, 20, width - 20);
 }
 
 function windowResized() {
 	resizeCanvas(
 		window.innerWidth,
 		window.innerHeight);
-}
-
-function drawCircle(x, y, radius) {
-	stroke(100);
-	noFill();
-	ellipse(x, y, radius, radius);
-	if (radius > 12) {
-		drawCircle(x + radius / 2, y, radius / 2);
-		drawCircle(x - radius / 2, y, radius / 2);
-		drawCircle(x, y + radius / 2, radius / 2);
-		drawCircle(x, y - radius / 2 , radius / 2);
 	}
-}
+
+	function drawCircle(x, y, radius) {
+		stroke(100);
+		noFill();
+		ellipse(x, y, radius, radius);
+		if (radius > 12) {
+			drawCircle(x + radius / 2, y, radius / 2);
+			drawCircle(x - radius / 2, y, radius / 2);
+			drawCircle(x, y + radius / 2, radius / 2);
+			drawCircle(x, y - radius / 2 , radius / 2);
+		}
+	}
+
+	function cantor(x, y, len) {
+		stroke(50);
+		strokeWeight(1);
+		noFill();
+		line(x, y, x + len, y);
+		ellipse(x + len / 2, y, len, len);
+
+		if (len > 2) {
+				let nextLen = len / 3;
+				y += 10;
+				cantor(x, y, nextLen);
+				cantor(x + nextLen * 2, y, nextLen);
+		}
+	}
